47bd51f220eff59ced63ba66f87a46de03496b97
Ubaidh Bin Hussain <65657505+SadiqLuther@users.noreply.github.com>
2021-03-01 13:00:38 +0530

    Update 2-clone-sample-html-web-app.sh

--- 2-clone-sample-html-web-app.sh
+++ 2-clone-sample-html-web-app.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd /$home/var/www/html/
-sudo git clone https://github.com/vpjaseem/sample-static-html-web-app.git
+sudo git clone https://github.com/SadiqLuther/sample-static-html-web-app.git
 sudo cp -r /var/www/html/sample-static-html-web-app/* /var/www/html/
 sudo rm -r sample-static-html-web-app
 sudo service httpd restart
